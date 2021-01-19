fc3a085b0df9834996259f60b30cb88914b5fca4
Nurumbet-ux <saryevnur@gmail.com>
2021-01-19 13:13:22 +0600

    commiyt

--- app/src/main/java/com/example/lifecycle/MainActivity.java
+++ app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.lifecycle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
@@ -13,13 +12,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView txtView1, txtView2;
-    private EditText etText1;
-    private EditText etText2;
-    private EditText etText3;
+    private TextView  txtView2,textView2;
+    private EditText etText1,etText2,etText3;
+    private String set;
     private double int1, int2, int3;
-    private String KEY;
-    private String KEYS;
+    public static String KEY="key";
+    public static String KEYS = "keys";
+    private String specific;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,51 +26,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
     }
-
-
     private void init() {
-        txtView1 = findViewById(R.id.Addition);
-        txtView2 = findViewById(R.id.Arithmetic);
-        etText1 = findViewById(R.id.EditText1);
-        etText2 = findViewById(R.id.EditText2);
-        etText3 = findViewById(R.id.EditText3);
+        txtView2 = findViewById(R.id.arithmetic);
+        etText1 = findViewById(R.id.editText1);
+        etText2 = findViewById(R.id.editText2);
+        etText3 = findViewById(R.id.editText3);
+        textView2 = findViewById(R.id.function);
     }
 
-    public void setClick2(View view) {
-        prove();
-        txtView2.setText((String) Double.valueOf(int1 + int2 / int3).toString());
-    }
-
-    public void prove() {
-        String num1 = etText1.getText().toString();
-        String num2 = etText2.getText().toString();
-        String num3 = etText3.getText().toString();
-        if (!(num1.equals("") && num2.equals("") && num3.equals(""))) {
-            int1 = Integer.parseInt(num1);
-            int2 = Integer.parseInt(num2);
-            int3 = Integer.parseInt(num3);
-
+    public void setClick(View view) {
+        if (!(etText1.getText().toString().equals("") && etText2.getText().toString().equals("") && etText3.getText().toString().equals(""))) {
+            int1 = Integer.parseInt(etText1.getText().toString());
+            int2 = Integer.parseInt(etText2.getText().toString());
+            int3 = Integer.parseInt(etText3.getText().toString());
         } else {
             Toast.makeText(this, "is empty!!", Toast.LENGTH_SHORT).show();
         }
+         set =  Double.valueOf(int1 + int2 + int3).toString();
+         specific = String.valueOf(3+4/2);
+         txtView2.setText(set);
+         textView2.setText(specific);
     }
 
 
-    public void setClick(View view) {
-        txtView1.setText((String) Double.valueOf(int1 + int2 + int3).toString());
-    }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(KEY, txtView1.getText().toString());
-        outState.putString(KEYS, txtView2.getText().toString());
+        outState.putString(KEY,set);
+        outState.putString(KEYS,specific);
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        txtView1.setText((String) savedInstanceState.get(KEY));
-        txtView2.setText((String) savedInstanceState.get(KEYS));
+        set = savedInstanceState.getString(KEY);
+        specific = savedInstanceState.getString(KEYS);
+        textView2.setText(specific);
+        txtView2.setText(set);
     }
+
+
 }
